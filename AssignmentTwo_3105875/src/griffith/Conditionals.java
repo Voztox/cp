461cb19fc9e3dd638adad09624b92ea56f5939bb
Voztox <117932184+Voztox@users.noreply.github.com>
2022-11-15 13:04:35 +0000

    Update Conditionals.java

--- AssignmentTwo_3105875/src/griffith/Conditionals.java
+++ AssignmentTwo_3105875/src/griffith/Conditionals.java
@@ -7,7 +7,7 @@ public class Conditionals {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
-		/*
+
 	System.out.println("Assignment Two Part One Task One");	
 	
 	System.out.println("Enter your height in meters and your weight in kilograms");
@@ -15,7 +15,6 @@ public class Conditionals {
 	double height = input.nextDouble();
 	double weight = input.nextDouble();
 	final double bmi = weight/(height*height);
-	
 	
 	if (bmi < 18.5) {
 		System.out.println("Your bmi is " + (Math.round(bmi)) + " you are underweight");
@@ -40,10 +39,12 @@ public class Conditionals {
 	System.out.println("--------------------------------");
 	System.out.println("Assignment Two Part One Task Two");	
 	
-	System.out.println("Enter a word to shift");
-	String word = input.nextLine();
 	System.out.println("Enter the amount of positions to shift");
 	int amount = input.nextInt();
+	System.out.println("Enter a word to shift");
+	String word = input.nextLine();
+	 word = input.nextLine();
+	
 	
 	String ciphertext = "";
 	
@@ -75,32 +76,40 @@ public class Conditionals {
 	        
 	    }
 	    System.out.println(ciphertext);
-		
 	
+
 	System.out.println("Assignment Two Part One Task Three");
 	System.out.println("--------------------------------");	
 	
 	System.out.println("Enter a number between 1 and 25");
 	long num = input.nextLong();
+	//after 20 long element does not help me and the number converts to negative that's why i used BigInteger
 	
-	if (num < 0)
+	while (num < 0 || num > 25)
 	{
-		System.out.println("Positive numbers only");
-		num = input.nextLong();
+		if (num < 0)
+		{
+			System.out.println("Positive numbers only");
+			System.out.println("Try again between 1-25");
+			num = input.nextLong();
+			// is BigInteger allowed? 
+		}
+		else if (num > 25)
+		{
+			System.out.println("Too large to print");
+			System.out.println("Try again between 1-25");
+			num = input.nextLong();
+		}
+		
 	}
-	else if (num > 25)
-	{
-		System.out.println("Too large to print");
-		num = input.nextLong();
-	}
-	
+		
 	long fact = 1;
 	for (int i=1; i <= num; i++)
 	{
 		fact=fact*i;
 	}
-	System.out.println("factorial " + fact);
-	*/	
+	System.out.println("The factorial of " + num + " is " + fact);
+	
 	System.out.println("Assignment Two Part One Task Four");
 	System.out.println("--------------------------------");	
 	
@@ -110,18 +119,64 @@ public class Conditionals {
 	{
 	case 1: 
 		System.out.println("You have choosen the easy mode");
-		
-		
-		break;
+        String[] operations = {"+", "-", "*", "/"};
+        int minOp = 0;
+        int maxOp = 3;
+        operations = (Math.random()*(maxOp-minOp)) + minOp;
+        
+        switch (difficulty) 
+        {
+        case 1: 
+            System.out.println("You have choosen the easy mode");
+            int min1 = 1;
+            int max1 = 10;
+            
+            for(int i = 0; i < 10; i++) 
+            {
+                int rngOne = (int) (Math.random()*(max1-min1)) + min1;
+                int rngTwo = (int) (Math.random()*(max1-min1)) + min1;
+                
+                System.out.println(rngOne + operations[operation] +  rngTwo);
+                
+                
+            }
+            break;
+	
 	case 2: 
 		System.out.println("You have choosen the hard mode");
-		if ()
+		int min2 = 1;
+        int max2 = 10;
+		
+		for(int i = 0; i < 10; i++) 
+		{
+			int rngOne = (int) (Math.random()*(max2-min2)) + min2;
+			int rngTwo = (int) (Math.random()*(max2-min2)) + min2;
+            System.out.println(rngOne +  +  rngTwo);
+		}
 		break;
 	case 3: 
 		System.out.println("You have choosen the easy elimination mode");
+		int min3 = 1;
+        int max3 = 10;
+		
+		for(int i = 0; i < 10; i++) 
+		{
+			int rngOne = (int) (Math.random()*(max3-min3)) + min3;
+			int rngTwo = (int) (Math.random()*(max3-min3)) + min3;
+            System.out.println(rngOne +  +  rngTwo);
+		}
 		break;
 	case 4: 
 		System.out.println("You have choosen the hard elimination mode");
+		int min4 = 1;
+        int max4 = 10;
+		
+		for(int i = 0; i < 10; i++) 
+		{
+			int rngOne = (int) (Math.random()*(max4-min4)) + min4;
+			int rngTwo = (int) (Math.random()*(max4-min4)) + min4;
+            System.out.println(rngOne +  +  rngTwo);
+		}
 		break;
 	default:
 		System.out.println("Not a valid number of difficulty. Please Try again between 1-4...");
